afe67d1309cca7b89987a013b0f28d0dc24aee6d
leftrightleft <leftrightleft@github.com>
2023-06-16 12:43:36 -0700

    file handling

--- get-billers.py
+++ get-billers.py
@@ -1,7 +1,9 @@
 import requests
 import os
+import json
 
 admin_token = os.environ.get('ADMIN_TOKEN')
+
 enterprise = os.environ.get('ENTERPRISE_NAME')
 
 def get_billing(enterprise):
@@ -11,14 +13,21 @@ def get_billing(enterprise):
         "Content-Type": "application/json"
     }
 
-    url=f"https://api.github.com/enterprises/{enterprise}/settings/billing/advanced-security"
+    url=f"https://api.github.com/enterprises/{enterprise}/settings/billing/advanced-security?per-page=100"
     response = requests.get(url, headers=headers)
-    
-    return response.json()
+    repos = response.json()
+    # build paging 
+    while 'next' in response.links.keys():
+        response = requests.get(response.links['next']['url'], headers=headers)
+        page_repos = response.json()
+        repos['repositories'].extend(page_repos['repositories'])
+
+    return repos
 
 def main():
     response = get_billing(enterprise)
-    print(response)
+    with open('GHAS-committers.json', 'w') as f:
+        json.dump(response, f)
 
 
 if __name__ == '__main__':
